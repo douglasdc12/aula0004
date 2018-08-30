9a56997506493c8cf1786e09ffaf06306dcf9873
douglasdc12 <42851711+douglasdc12@users.noreply.github.com>
2018-08-30 20:31:48 -0300

    Add files via upload

--- JavaApplication18.java
+++ JavaApplication18.java
@@ -12,43 +12,44 @@ public class JavaApplication18 {
       
       System.out.println("digite um numero");
       numero = inp.nextInt();
-       
-      
+
               switch (numero){
           case 1 : 
               System.out.println("janeiro");
-              
+              break;
           case 2 :
               System.out.println("fevereiro");
-                          
+             break;             
           case 3 :
-              System.out.println("marco");
-             
+              System.out.println("março");
+             break;
           case 4 :
               System.out.println("abril");
-             
+             break;
             case 5 :
               System.out.println("maio");
-                
+                break;
            case 6 :
               System.out.println("junho");
-              
+              break;
               case 7 :
               System.out.println("julho");
-              
+              break;
               case 8 :
               System.out.println("agosto");
-             
+             break;
               case 9 :
               System.out.println("Setembro");
-              
+              break;
               case 10 :
               System.out.println("outubro");
+              break;
               case 11 :
               System.out.println("novembro");
-              
+              break;
               case 12 :
               System.out.println("dezembro");
+              break;
         }
     
           
