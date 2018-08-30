40545ec129561aa5ffcad5310b95c937f4988d75
douglasdc12 <42851711+douglasdc12@users.noreply.github.com>
2018-08-30 20:21:52 -0300

    Add files via upload

---
+++ JavaApplication18.java
@@ -0,0 +1,60 @@
+
+package javaapplication18;
+
+import java.util.Scanner;
+
+
+public class JavaApplication18 {
+
+    public static void main(String[] args) {
+      Scanner inp = new Scanner(System.in);
+      int numero;
+      
+      System.out.println("digite um numero");
+      numero = inp.nextInt();
+       
+      
+              switch (numero){
+          case 1 : 
+              System.out.println("janeiro");
+              
+          case 2 :
+              System.out.println("fevereiro");
+                          
+          case 3 :
+              System.out.println("marco");
+             
+          case 4 :
+              System.out.println("abril");
+             
+            case 5 :
+              System.out.println("maio");
+                
+           case 6 :
+              System.out.println("junho");
+              
+              case 7 :
+              System.out.println("julho");
+              
+              case 8 :
+              System.out.println("agosto");
+             
+              case 9 :
+              System.out.println("Setembro");
+              
+              case 10 :
+              System.out.println("outubro");
+              case 11 :
+              System.out.println("novembro");
+              
+              case 12 :
+              System.out.println("dezembro");
+        }
+    
+          
+              
+      }
+      
+           
+    }
+    
